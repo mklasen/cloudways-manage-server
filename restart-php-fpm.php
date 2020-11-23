5b7af00340f0c8002ad133dc94a77d1a56cf1508
Marinus Klasen <marinus@mklasen.nl>
2020-11-23 15:09:17 +0100

    Get server_id from hostname and use it

--- restart-php-fpm.php
+++ restart-php-fpm.php
@@ -6,7 +6,11 @@ $servers = $service->getServers();
 foreach ( $servers as $server ) {
 	if ( is_array( $server ) && ! empty( $server ) ) {
 		$object = reset( $server );
-		if ( isset( $_SERVER['USER'] ) && $object->label === $_SERVER['USER'] ) {
+
+		$hostname = gethostname();
+		$server_id = preg_replace('/\D/', '', $hostname);
+
+		if ( $object->id === $server_id ) {
 			$get_services = $service->getServices( $object->id );
 			$running_services     = reset( $get_services->services );
 			foreach ( $running_services as $key => $running_service ) {
