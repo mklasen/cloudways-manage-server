5046cbb0210d3d4a64d53493483d5841bd7af043
Marinus Klasen <marinus@mklasen.nl>
2020-11-23 15:13:48 +0100

    Don't need to loop through servers anymore

--- restart-php-fpm.php
+++ restart-php-fpm.php
@@ -1,23 +1,13 @@
 <?php
 require 'main.php';
 
-$servers = $service->getServers();
+$hostname  = gethostname();
+$server_id = preg_replace( '/\D/', '', $hostname );
 
-foreach ( $servers as $server ) {
-	if ( is_array( $server ) && ! empty( $server ) ) {
-		$object = reset( $server );
-
-		$hostname = gethostname();
-		$server_id = preg_replace('/\D/', '', $hostname);
-
-		if ( $object->id === $server_id ) {
-			$get_services = $service->getServices( $object->id );
-			$running_services     = reset( $get_services->services );
-			foreach ( $running_services as $key => $running_service ) {
-				if ( strpos( $key, '-fpm' ) !== false ) {
-					$service->manageServices( $object->id, $key, 'restart' );
-				}
-			}
-		}
+$get_services     = $service->getServices( $server->id );
+$running_services = reset( $get_services->services );
+foreach ( $running_services as $key => $running_service ) {
+	if ( strpos( $key, '-fpm' ) !== false ) {
+		$service->manageServices( $server->id, $key, 'restart' );
 	}
 }
