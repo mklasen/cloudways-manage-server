fabe71c6ca1f2f6753df23a33ea3de9aeeadcfdd
Marinus Klasen <marinus@mklasen.nl>
2020-11-26 14:49:50 +0100

    Initial

--- restart-php-fpm.php
+++ restart-php-fpm.php
@@ -1,13 +1,23 @@
 <?php
 require 'main.php';
 
-$hostname  = gethostname();
-$server_id = preg_replace( '/\D/', '', $hostname );
+use function Deployer\get;
+use function Deployer\desc;
+use function Deployer\task;
 
-$get_services     = $service->getServices( $server_id );
-$running_services = reset( $get_services->services );
-foreach ( $running_services as $key => $running_service ) {
-	if ( strpos( $key, '-fpm' ) !== false ) {
-		$service->manageServices( $server_id, $key, 'restart' );
+desc( 'Restart PHP-FPM on Cloudways server' );
+task(
+	'deploy:restart-php-fpm',
+	function() {
+		$hostname  = gethostname();
+		$server_id = preg_replace( '/\D/', '', $hostname );
+
+		$get_services     = $service->getServices( $server_id );
+		$running_services = reset( $get_services->services );
+		foreach ( $running_services as $key => $running_service ) {
+			if ( strpos( $key, '-fpm' ) !== false ) {
+				$service->manageServices( $server_id, $key, 'restart' );
+			}
+		}
 	}
-}
+);
